4af97e3fe335e6c6d5f0aa90a449950779febd45
krashdar <75021815+krashdar@users.noreply.github.com>
2021-06-06 13:08:01 +0700

    Update zadanie2.js

--- zadanie2/zadanie2.js
+++ zadanie2/zadanie2.js
@@ -1,3 +1,4 @@
+//Сверстайте кнопку, клик на которую будет выводить данные о размерах экрана с помощью alert. 
 const btn = document.querySelector('.j-btn');
 btn.addEventListener('click', () =>{
     const size = `Текущее разрешение экрана: \n Ширина - ${window.screen.width}px \n Высота - ${window.screen.height}px`;
